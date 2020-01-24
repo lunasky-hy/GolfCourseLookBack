46541b19eb30f5d7c61f9e33e7cab3ee82583914
Arpedia <planes.next1@gmail.com>
2020-01-25 08:14:41 +0900

    initial commit

---
+++ ClientApp/src/components/Search.js
@@ -0,0 +1,22 @@
+﻿import React, { Component } from 'react';
+
+export class Search extends Component {
+    static displayName = Search.name;
+
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>ゴルフ場の検索</h1>
+                <form>
+                    検索ワード：
+                    <input type="text" name="name" />
+                </form>
+            </div>
+        );
+    }
+}
