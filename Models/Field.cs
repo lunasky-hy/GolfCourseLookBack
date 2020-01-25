03084776f31aa02c48a59998a5dcfbbcd798a5aa
Arpedia <planes.next1@gmail.com>
2020-01-25 13:21:36 +0900

    model 追加

---
+++ Models/Field.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GolfCourseLookBack.Models
+{
+    public class Field
+    {
+        public int Id { get; set; }
+        public int ShotNaviId { get; set; }
+        public string Name { get; set; }
+        public string CourseList { get; set; }
+        public string CourseURLId { get; set; }
+    }
+}
